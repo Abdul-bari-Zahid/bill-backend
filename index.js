a934c6a069d152025e3d9fbf7977599b565377bb
Abdul-bari-Zahid <bariblogger6@gmail.com>
2026-02-12 17:08:42 +0500

    first thing

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import billSuggestionRoutes from "./routes/billSuggestionRoutes.js";
 dotenv.config();
 const app = express();
 
-app.use(cors({ origin: ["http://localhost:5173", "https://ai-doc-client.vercel.app"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173", "https://bill-frontend-omega.vercel.app/"], credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
